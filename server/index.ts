71e82e217c0512d9eea7e497288b0335a329deb9
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-03-29 14:01:37 +0200

    init server

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+import express, { Express } from 'express';
+// import cors from 'cors';
+// import cookieParser from 'cookie-parser';
+// import router from '../routes';
+// import errorMiddleware from '../middleware/error';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.use(
+//   cors({
+//     origin: [process.env.CLIENT_URL, process.env.ADMIN_URL] as string[],
+//     credentials: true,
+//     methods: 'GET, POST, PUT, PATCH, DELETE',
+//     allowedHeaders: 'Content-Type, Authorization, Set-Cookie',
+//     exposedHeaders: 'X-Total-Count',
+//   }),
+// );
+
+// app.use(router);
+
+// app.use(errorMiddleware);
+
+app.get('/', (_req, res) => {
+  res.send('It works');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+  return console.log(`Server is listening at http://localhost:${process.env.SERVER_PORT}`);
+});
