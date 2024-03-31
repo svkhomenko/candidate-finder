9c5b350f7464cb9196fe93abbc7e5912fee6b917
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-03-31 14:51:39 +0300

    init prisma

---
+++ server/prisma/migrations/20240331114836_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `resume` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
