6633dc5cb169d6f75de7a2c0a065edced626ae1a
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-03-31 15:15:01 +0300

    create resume post controller

--- server/controllers/resumes.ts
+++ server/controllers/resumes.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
+import prisma from '../lib/prisma';
+
+const resume = prisma.resume;
 
 const createResume = async (req: Request, res: Response) => {
   const data = req.body;
-
-  res.status(201).json(data);
+  const newResume = await resume.create({
+    data: data,
+  });
+  res.status(201).json(newResume);
 };
 
 export { createResume };
