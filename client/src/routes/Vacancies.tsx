713f0c8805af1200018e35c311403789bdc67b7c
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-04-03 00:30:23 +0300

    add vacancy create page

--- client/src/routes/Vacancies.tsx
+++ client/src/routes/Vacancies.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import VacancyPage from '~/pages/VacancyPages/VacancyPage';
-// import VacancyCreateForm from '~/pages/VacancyPages/VacancyCreate/VacancyCreateForm';
+import VacancyCreateForm from '~/pages/VacancyPages/VacancyCreate/VacancyCreateForm';
 import NotFound from '~/pages/NotFound/NotFound';
 
 const VacanciesRoutes = () => (
   <Routes>
     <Route path="/:id" element={<VacancyPage />} />
-    {/* <Route path="/create" element={<VacancyCreateForm />} /> */}
+    <Route path="/create" element={<VacancyCreateForm />} />
     <Route path="/*" element={<NotFound />} />
   </Routes>
 );
--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -1,26 +1,31 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
 
-const theme = extendTheme({
-  fonts: {
-    heading: `"Inter", sans-serif`,
-    body: `"Inter", sans-serif`,
-  },
-  semanticTokens: {
-    colors: {
-      secondary: {
-        default: 'green.500',
-      },
-      hover: {
-        default: 'green.800',
-      },
-      tertiary: {
-        default: 'teal.500',
-      },
-      text: {
-        default: 'white',
+const theme = extendTheme(
+  {
+    fonts: {
+      heading: `"Inter", sans-serif`,
+      body: `"Inter", sans-serif`,
+    },
+    semanticTokens: {
+      colors: {
+        secondary: {
+          default: 'green.500',
+        },
+        hover: {
+          default: 'green.800',
+        },
+        tertiary: {
+          default: 'teal.500',
+        },
+        text: {
+          default: 'white',
+        },
       },
     },
   },
-});
+  withDefaultColorScheme({
+    colorScheme: 'green',
+  }),
+);
 
 export default theme;
