f5c307562acb126bbc1da6cdeaa368af72081085
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-04-02 12:34:18 +0300

    update page alert style

--- client/src/components/PageAlert/page-alert.styles.ts
+++ client/src/components/PageAlert/page-alert.styles.ts
@@ -10,6 +10,32 @@ const styles = {
     width: '100%',
     borderRadius: '10px',
     padding: '30px 20px',
+    border: '3px solid',
+
+    '&[data-status="info"]': {
+      background: 'blue.50',
+      borderColor: 'blue.500',
+    },
+
+    '&[data-status="warning"]': {
+      background: 'yellow.50',
+      borderColor: 'yellow.500',
+    },
+
+    '&[data-status="success"]': {
+      background: 'green.50',
+      borderColor: 'green.500',
+    },
+
+    '&[data-status="error"]': {
+      background: 'red.50',
+      borderColor: 'red.500',
+    },
+
+    '&[data-status="loading"]': {
+      background: 'blue.50',
+      borderColor: 'blue.500',
+    },
   },
 };
 
