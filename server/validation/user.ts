457a93e89ee33d90b2cc565d93d3706dcd2d656f
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-05-04 19:03:59 +0300

    add auth validation

---
+++ server/validation/user.ts
@@ -0,0 +1,21 @@
+import Joi from 'joi';
+import { FULL_NAME_LENGTH, PASSWORD_LENGTH, ROLE_ENUM } from '../consts/validation';
+
+const JoiWithPhoneNumber = Joi.extend(require('joi-phone-number'));
+
+const loginSchema = Joi.object().keys({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const registerSchema = JoiWithPhoneNumber.object().keys({
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(PASSWORD_LENGTH.min).max(PASSWORD_LENGTH.max),
+  fullName: Joi.string().required().min(FULL_NAME_LENGTH.min).max(FULL_NAME_LENGTH.max),
+  role: Joi.string()
+    .valid(...ROLE_ENUM)
+    .required(),
+  phoneNumber: Joi.string().phoneNumber().required(),
+});
+
+export { loginSchema, registerSchema };
