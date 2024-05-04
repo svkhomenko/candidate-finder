2e5d63180bad9091528371b1ead62a186b5342f2
Svetlana Khomenko <hsvetlana03@gmail.com>
2024-05-04 19:33:29 +0300

    fix error with phone number

--- server/validation/user.ts
+++ server/validation/user.ts
@@ -8,14 +8,14 @@ const loginSchema = Joi.object().keys({
   password: Joi.string().required(),
 });
 
-const registerSchema = JoiWithPhoneNumber.object().keys({
+const registerSchema = Joi.object().keys({
   email: Joi.string().email().required(),
   password: Joi.string().required().min(PASSWORD_LENGTH.min).max(PASSWORD_LENGTH.max),
   fullName: Joi.string().required().min(FULL_NAME_LENGTH.min).max(FULL_NAME_LENGTH.max),
   role: Joi.string()
     .valid(...ROLE_ENUM)
     .required(),
-  phoneNumber: Joi.string().phoneNumber().required(),
+  phoneNumber: JoiWithPhoneNumber.string().phoneNumber().required(),
 });
 
 export { loginSchema, registerSchema };
